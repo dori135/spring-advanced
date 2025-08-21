678d9425b4f9b055c1505bd7ccdcf2585dbe548b
dori135 <nolli77@ewha.ac.kr>
2025-08-21 15:35:44 +0900

    레벨 2: N+1 문제 (@EntityGraph 기반 구현)

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -65,7 +65,7 @@ public class TodoService {
     }
 
     public TodoResponse getTodo(long todoId) {
-        Todo todo = todoRepository.findByIdWithUser(todoId)
+        Todo todo = todoRepository.findById(todoId) // findByIdWithUser -> findById로 변경
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
         User user = todo.getUser();
